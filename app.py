f928404aa238381864038b0c15898f093e11f942
GaelLaraBautista <larabautistagael@gmail.com>
2025-02-20 22:20:05 -0600

    03 Implementando Lógica Para El Guardado e Inicio de Sesión De Los Usuarios

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
+from flask_cors import CORS
+import datetime
 import os
 
 app = Flask(__name__)
@@ -11,12 +14,17 @@ app.config ['MONGO_URI'] = os.getenv('MONGO_URI')
 
 #### Conexión a Base de Datos Con MongoDB ###
 mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
 
 #### Rutas Ordinarias ####
 
 @app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/inicio')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/contacto')
 def contacto():
@@ -24,12 +32,41 @@ def contacto():
 
 ##### Rutas de Registro #######
 
-@app.route('/login')
+@app.route('/login', methods=["POST"])
 def login():
- return render_template('login.html')
+    username = request.form["username"]
+    password = request.form["password"]
 
-@app.route('/signup')
+    user = mongo.db.users.find_one({"username": username})
+    if user and bcrypt.check_password_hash(user["password"], password):  # Validar hash correctamente
+        session["user"] = user["username"]
+        return redirect(url_for("index"))
+    
+    return jsonify({"message": "Credenciales incorrectas"}), 401
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+   if request.method == "POST":
+      ### Parametros a guardar en la Base de datos ####
+      username = request.form["username"]
+      email = request.form["email"]
+      password = request.form["password"]
+      #### Encriptado de la contraseña ###
+      hashed_pw = bcrypt.generate_password_hash(password).decode("utf-8")
+
+      if mongo.db.users.find_one({"email": email}):
+         return jsonify({"message": "Este correo ya existe"})
+   
+      user_data = {
+         "username": username,
+         "email" : email,
+         "password": hashed_pw,
+         "create_at": datetime.datetime.utcnow()
+      }
+
+      mongo.db.users.insert_one(user_data)
+      return redirect(url_for('home'))
+
    return render_template('signup.html')
 
 #### Rutas de Tareas ###
