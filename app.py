07b3cd6c7cab71c90d55a0945cbdc18d433b415b
GaelLaraBautista <larabautistagael@gmail.com>
2025-02-20 20:12:28 -0600

    02 Implementando Formularios & Conexión con la Base de Datos

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'super_secreto_seguro'
+
+#### Conexión a Base de Datos Con MongoDB ###
+
+app.config['MONGO_URI'] = "mongodb+srv://larabautistagael:xlg36au6xU53O9zF@neobytedb.bipzm.mongodb.net/neobyteDB"
+mongo = PyMongo(app)
+
+#### Rutas Ordinarias ####
 
 @app.route('/')
 def index():
@@ -10,15 +20,17 @@ def index():
 def contacto():
    return render_template('contacto.html')
 
-##### rutas de Registro #######
+##### Rutas de Registro #######
 
 @app.route('/login')
 def login():
  return render_template('login.html')
 
-@app.route('signup')
+@app.route('/signup')
 def signup():
    return render_template('signup.html')
+
+#### Rutas de Tareas ###
 
 
 if __name__ == '__main__':
