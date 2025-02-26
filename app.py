0dfdc07d67341c2116cf80691c138fdecbb7f2f5
GaelLaraBautista <larabautistagael@gmail.com>
2025-02-26 13:49:41 -0600

    05 Implementando Dashboard

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for, session, flash
 from flask_pymongo import PyMongo
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_bcrypt import Bcrypt
+from passlib.context import CryptContext  # Usamos Passlib para el manejo de contraseñas
 from dotenv import load_dotenv
 from flask_cors import CORS
-import datetime
+from datetime import datetime
 import os
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 load_dotenv()
 app.secret_key = os.getenv('SECRET_KEY')
-app.config ['MONGO_URI'] = os.getenv('MONGO_URI')
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
 #### Conexión a Base de Datos Con MongoDB ###
 mongo = PyMongo(app)
-bcrypt = Bcrypt(app)
+
+# Inicializamos el contexto de Passlib
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 #### Rutas Ordinarias ####
 
@@ -25,7 +26,25 @@ def home():
 
 @app.route('/inicio')
 def index():
-    return render_template('index.html')
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    
+    #### Obtener metricas de Mongo DB ######
+    total_users = mongo.db.users.count_documents({})
+    pending_tasks = mongo.db.tasks.count_documents({"status": "pendiente"})
+    new_messages = mongo.db.messages.count_documents({"read": False})
+
+    #### Actividad Reciente ######
+
+    recent_activity = list(mongo.db.activity_logs.find().sort("timestamp", -1).limit(5))
+
+    return render_template(
+   'index.html',
+    total_users=total_users,
+    pending_task=pending_tasks,
+    new_messages=new_messages,
+    recent_activity=recent_activity
+    )
 
 @app.route('/contacto')
 def contacto():
@@ -39,11 +58,16 @@ def login():
     password = request.form["password"]
 
     user = mongo.db.users.find_one({"username": username})
-    if user and bcrypt.check_password_hash(user["password"], password):  # Validar hash correctamente
-        session["user"] = user["username"]
-        return redirect(url_for("index"))
-    
-    return jsonify({"message": "Credenciales incorrectas"}), 401
+    if user:
+        # Verificar si la contraseña es válida con Passlib
+        if pwd_context.verify(password, user["password"]):  # Verificar contraseña en hash
+            session["user"] = user["username"]
+            return redirect(url_for("index"))
+        else:
+            return jsonify({"message": "Credenciales incorrectas"}), 401
+    else:
+        return jsonify({"message": "Usuario no encontrado"}), 404
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -53,18 +77,18 @@ def signup():
       email = request.form["email"]
       password = request.form["password"]
       #### Encriptado de la contraseña ###
-      hashed_pw = bcrypt.generate_password_hash(password).decode("utf-8")
+      hashed_pw = pwd_context.hash(password)  # Encriptar la contraseña con Passlib
 
       if mongo.db.users.find_one({"email": email}):
          return jsonify({"message": "Este correo ya existe"})
    
       user_data = {
          "username": username,
-         "email" : email,
+         "email": email,
          "password": hashed_pw,
          "profile_picture": "",
          "create_at": datetime.datetime.utcnow(),
-         "updated_at": datetime.datetime.utcnow() 
+         "updated_at": datetime.datetime.utcnow()
       }
 
       mongo.db.users.insert_one(user_data)
@@ -81,7 +105,7 @@ def logout():
 def change_password():
 
    if 'user' not in session:
-      return redirect( url_for ('home'))
+      return redirect(url_for('home'))
 
    if request.method == 'POST':
       current_password = request.form.get('current_password')
@@ -90,59 +114,68 @@ def change_password():
 
       user = mongo.db.users.find_one({"username": session['user']})
 
-      if not check_password_hash(user['password'], current_password):
+      # Verificar si la contraseña actual es correcta
+      if not pwd_context.verify(current_password, user['password']):
          flash('La contraseña actual es Incorrecta', "error")
-         return redirect(url_for('change_passowrd'))
+         return redirect(url_for('change_password'))
       
       if new_password != confirm_password:
          flash('Las Contraseñas No Coinciden', "error")
-         return redirect( url_for ('change_password'))
+         return redirect(url_for('change_password'))
       
-      hashed_password = generate_password_hash(new_password)
+      # Encriptar la nueva contraseña
+      hashed_password = pwd_context.hash(new_password)
       mongo.db.users.update_one(
          {"username": session['user']},
-         {"$set":{"password": hashed_password}}
+         {"$set": {"password": hashed_password}}
       )
       flash('Contraseña Cambiada Correctamente', "success")
-      return redirect( url_for ('profile'))
-   return render_template ('change_password.html')
+      return redirect(url_for('profile'))
+   return render_template('change_password.html')
 
 #### Rutas del perfil del Usuario ###
 
 @app.route('/profile')
 def profile():
-    
     if 'user' not in session:
         return redirect(url_for('home'))
     
     user = mongo.db.users.find_one({"username": session['user']})
-    return render_template ('profile.html', user=user)
+    return render_template('profile.html', user=user)
 
 @app.route('/profile/edit', methods=['GET', 'POST'])
 def edit_profile():
-    
     if 'user' not in session:
-     return redirect(url_for('home'))
+        return redirect(url_for('home'))
     
     user = mongo.db.users.find_one({"username": session['user']})
 
     if request.method == 'POST':
-       username = request.form.get('username')
-       email = request.form.get('email')
-       profile_picture = request.form.get('profile_picture')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        profile_picture = request.form.get('profile_picture')
 
-       mongo.db.users.update_one(
-         {"username": session['user']},
-         {"$set":{
-            "username": username,
-            "email": email,
-            "profile_picture": profile_picture,
-            "update_at": datetime.utcnow()
-         }}
-       )
-       flash ("Perfil Actualizado Correctamente", "success")
-       return redirect(url_for('profile'))
-    return render_template('profile.html', user=user)
+        print(f"Updating user: {username}, {email}, {profile_picture}")  # Depuración
+
+        mongo.db.users.update_one(
+            {"username": session['user']},
+            {"$set": {
+                "username": username,
+                "email": email,
+                "profile_picture": profile_picture,
+                "updated_at": datetime.utcnow()
+            }}
+        )
+
+        session['user'] = username  
+
+        flash("Perfil Actualizado Correctamente", "success")
+        return redirect(url_for('profile')) 
+
+    return render_template('profile_edit.html', user=user)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
